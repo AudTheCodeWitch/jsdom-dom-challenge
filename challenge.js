9490a2b294e97139d9fc324ab956e26a469572c3
Audrea Cook <audreacwork@gmail.com>
2019-10-16 11:04:56 -0600

    Add incrementCounter on + click

--- challenge.js
+++ challenge.js
@@ -11,5 +11,18 @@ const startCounter = function startCounter() {
         number.innerText = counter.toString();
 
     }, 1000);
-    console.log("about to start");
+    console.log("Counter Started");
+};
+
+const plus = document.getElementById('plus');
+
+plus.addEventListener("click", function() {
+    incrementCounter()
+} );
+
+const incrementCounter = function incrementCounter() {
+    counter ++;
+    number.innerText = counter.toString()
+    console.log('incremented')
+
 };
